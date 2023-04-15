dffb26ba32b10206f90768765af4fec3b34e8b61
Shiva <dasshivashish11@gmail.com>
2023-04-15 20:34:36 +0530

    First commit

---
+++ src/lang.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+
+void error(char* what, char* code, int line) {
+	printf("In line %d - %s\n", line, code);
+	printf("Error :%s", what);
+	exit(1);
+}
+
+static void RunFile(char* name) {
+	FILE* file = fopen(name, "rb");
+	if (!file) {
+		printf("Can't open %s", name);
+		exit(1);
+	}
+	fseek(file, 0, SEEK_END);
+	long end = ftell(file);
+	fseek(file, 0, SEEK_SET);
+	char* buf = malloc(sizeof(char) * end + 1);
+	buf[end] = '\0';
+	fread(buf, end, sizeof(char), file);
+	fclose(file);
+	printf("%s", buf);
+}
+
+int main(int argc, char *args[]) {
+    if (argc > 2) {
+	    printf("Usage lang [file]\n");
+	    printf("Need filename!");
+	    exit(64);
+    }
+    else if (argc == 2) {
+	    RunFile(args[1]);
+    }
+    else {
+	    //RunPrompt();
+    }
+    return 0;
+}
